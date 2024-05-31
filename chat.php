fe1fab9be06985d3bb33cb429b564b6a1b83ea95
Octave <55949956+Octaveeee@users.noreply.github.com>
2024-05-31 18:41:33 +0200

    Update chat.php

--- chat.php
+++ chat.php
@@ -10,7 +10,8 @@ function getPrenomFromDatabase() {
     // Si le BDD existe, faire le traitement
     if ($db_found) {
         // Exécuter la requête SQL pour obtenir le prénom
-        $sql = "SELECT Prenom FROM Client WHERE Numero_etudiant = 2";
+        // $sql = "SELECT Prenom FROM Client WHERE Numero_etudiant = 1";
+        $sql = "SELECT Prenom FROM Coach WHERE ID = 13";
         $result = mysqli_query($db_handle, $sql);
         // Vérifier si la requête a retourné un résultat
         if ($result && mysqli_num_rows($result) > 0) {
@@ -29,6 +30,38 @@ function getPrenomFromDatabase() {
     return null;
 }
 
+
+function PrenomDestinataire()
+{
+    // Identifier le nom de base de données
+    $database = "projetpiscine";
+    // Connectez-vous à votre BDD
+    $db_handle = mysqli_connect('localhost', 'root', 'root');
+    $db_found = mysqli_select_db($db_handle, $database);
+    // Si le BDD existe, faire le traitement
+    if ($db_found) {
+        // Exécuter la requête SQL pour obtenir le prénom
+        // $sql = "SELECT Prenom FROM Coach WHERE ID = 13";
+        $sql = "SELECT Prenom FROM Client WHERE Numero_etudiant = 1";
+        $result = mysqli_query($db_handle, $sql);
+        // Vérifier si la requête a retourné un résultat
+        if ($result && mysqli_num_rows($result) > 0) {
+            // Récupérer le prénom
+            $data = mysqli_fetch_assoc($result);
+            mysqli_close($db_handle);
+            return $data['Prenom'];
+        } else {
+            echo "Aucun étudiant trouvé avec le numéro d'étudiant 1.";
+        }
+    } else {
+        echo "Database not found";
+    }
+    // Fermer la connexion
+    mysqli_close($db_handle);
+    return null;
+
+
+}
 
 
 // Récupérer le prénom de l'étudiant et le stocker dans la session si ce n'est pas déjà fait
@@ -56,7 +89,10 @@ if (!isset($_SESSION['name'])) {
         <button id="Boutton_Apparition">CHATBOX</button>
         <?php
             $prenom = getPrenomFromDatabase();
-            $_SESSION['name'] = $prenom;         
+            $Prenom_dest = PrenomDestinataire();
+            $_SESSION['name'] = $prenom;
+            $_SESSION['nameDest'] = $Prenom_dest;     
+
         ?>
 
         <script>
@@ -71,13 +107,14 @@ if (!isset($_SESSION['name'])) {
 
         <div id="wrapper">
             <div id="menu">
-                <p class="welcome">Bienvenue, <b><?php echo $_SESSION['name']; ?></b></p>
+                <p class="TOP">Destinataire : <b><?php echo $_SESSION['nameDest']; ?></b></p>
                 
             </div>
             <div id="chatbox">
                 <?php
-                    if (file_exists($_SESSION['name']."log.html") && filesize($_SESSION['name']."log.html") > 0) {
-                        $contents = file_get_contents($_SESSION['name']."log.html");
+                    $filename = strtolower(min($_SESSION['name'], $_SESSION['nameDest']) . '_' . max($_SESSION['name'], $_SESSION['nameDest']) . '_log.html');
+                    if (file_exists($filename) && filesize($filename) > 0) {
+                        $contents = file_get_contents($filename);
                         echo $contents;
                     }
                 ?>
@@ -101,18 +138,18 @@ if (!isset($_SESSION['name'])) {
                 function loadLog() {
                     var oldscrollHeight = $("#chatbox")[0].scrollHeight - 20; // Hauteur de défilement avant la requête
                     $.ajax({
-                        url: "<?php echo $_SESSION['name']; ?>_log.html",
+                        url: "<?php echo $filename; ?>",
                         cache: false,
                         success: function (html) {
-                            $("#chatbox").html(html); // Insérez le log de chat dans la #chatbox div
-                            // Auto-scroll
-                            var newscrollHeight = $("#chatbox")[0].scrollHeight - 20; // Hauteur de défilement apres la requête
+                            $("#chatbox").html(html);
+                            var newscrollHeight = $("#chatbox")[0].scrollHeight - 20;
                             if (newscrollHeight > oldscrollHeight) {
-                                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); // Défilement automatique
+                                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal');
                             }
                         }
                     });
-                }
+
+                  }
 
                 setInterval(loadLog, 2500);
                 $("#exit").click(function () {
